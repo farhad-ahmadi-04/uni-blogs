88dfb04dc3523e594544ab725245cc7cd4a46c68
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-01-01 02:22:44 +0330

    feat(component): create themeswitch component
    Des:
    to set theme in localStorage and set theme by user choose

---
+++ src/components/ThemeSwitcher.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import {
+  HiOutlineSun as SunIcon,
+  HiOutlineMoon as MoonIcon,
+} from "react-icons/hi";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+
+export default function ThemeSwitch() {
+  const [mounted, setMounted] = useState(false);
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
+  return (
+    <button
+      onClick={() => setTheme(currentTheme === "dark" ? "light" : "dark")}
+    >
+      {currentTheme === "dark" ? (
+        <>
+          <SunIcon className="h-6 w-6" />
+          <span>Light</span>
+        </>
+      ) : (
+        <>
+          <MoonIcon className="h-6 w-6" />
+          <span>Dark</span>
+        </>
+      )}
+    </button>
+  );
+}
