de5b29411c1ed388125486ce6ee48163b337286d
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-01-02 01:36:50 +0330

    refactor(page): landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <>
+    <div id="root" className="h-full container m-auto flex flex-col text-primary-200">
       <Navbar />
-      <main>
+      <main className="h-full">
         <Hero />
       </main>
-    </>
+    </div>
   );
 }
