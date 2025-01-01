a2a9b821bfd63b793928f97b38b392af8f0b347d
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-01-02 01:29:38 +0330

    add(styled): global.css file
    Des:
    add colors to root and tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   darkMode: 'class', // Enable dark mode with class strategy
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        primary: 'var(--text-primary)',
+        primary: {
+          DEFAULT: 'var(--text-primary-100)',
+          '200': 'var(--text-primary-200)',
+          '300': 'var(--text-primary-300)',
+        },
         secondary: 'var(--text-secondary)',
         accent: 'var(--text-accent)',
       },
-      backgroundColor:{
+      backgroundColor: {
         primary: 'var(--bg-primary)',
-        screen: 'var(--bg-screen)'
-      }
+        screen: 'var(--bg-screen)',
+      },
     },
   },
-  plugins: [require('daisyui'),],
+  plugins: [require('daisyui')],
   daisyui: {
     themes: false, // غیرفعال کردن تم‌های پیش‌فرض DaisyUI
   },
