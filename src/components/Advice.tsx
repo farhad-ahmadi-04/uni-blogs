96ddd8bc880f1eb77ac88bf7cbfc89f425e17090
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-01-02 19:21:40 +0330

    update(component): add motion
    Des:
    add motion when div in viewPort when user drag and when user hover

--- src/components/Advice.tsx
+++ src/components/Advice.tsx
@@ -1,16 +1,62 @@
+"use client"
 import getData from '@/service/getData.mts'
+import { ApiResponse } from '@/types/types'
+import * as motion from "motion/react-client"
+import { useEffect, useRef, useState } from 'react'
 
-async function Advice() {
-    const data = await getData("https://api.adviceslip.com/advice")
+const box = {
+    width: 70,
+    height: 70,
+    backgroundColor: "var(--bg-primary)",
+    borderRadius: 5,
+}
+
+function Advice() {
+    const [data, setData] = useState<ApiResponse>()
+    const ref = useRef<HTMLDivElement>(null)
+
+
+    useEffect(function () {
+        const fetch = async () => {
+            const data = await getData("https://api.adviceslip.com/advice")
+            setData(data)
+        }
+        fetch()
+
+        return function () {
+            fetch()
+        }
+    }, [])
+
 
 
     return (
-        <div className="hero h-4/5 p-6 ">
+        <div className="hero h-4/5 p-6 " >
             <div className="flex flex-col items-center justify-center gap-5 bg-secondary rounded 
-            shadow-lg w-full h-full shadow-inner">
-                <h1 className="text-5xl font-bold">Advice from big man:)</h1>
-                <p>{data.slip.advice}</p>
-                <button className="btn btn-info">It&lsquo;s nothing...</button>
+            shadow-lg w-full h-full shadow-inner" >
+                <h1 className="text-5xl font-bold mt-8">Advice from big man:)</h1>
+                <p>{data?.slip.advice}</p>
+
+
+                <motion.div className='w-6/12 h-full' ref={ref}>
+                    <motion.div
+                        style={box}
+                        whileHover={{ borderRadius: 50 }}
+                        whileTap={{ scale: 0.95 }}
+                        initial={{ opacity: 0 }}
+                        whileInView={{ scale: 1.5 }}
+                        viewport={{ root: ref }}
+                        drag
+                        whileDrag={{ scale: 1.2, backgroundColor: "var(--bg-primary)" }}
+                        dragConstraints={ref}
+                        dragElastic={0.2}
+                        animate={{
+                            opacity: .6,
+                            transition: { duration: 2, }
+                        }}
+                    />
+                </motion.div>
+
             </div>
         </div >
     );
