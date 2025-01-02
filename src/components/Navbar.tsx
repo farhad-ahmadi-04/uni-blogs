689877789412fa8ee6ac87ed870726fab496f691
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-01-02 19:22:24 +0330

    update(component): Navbar
    Des:
    add class to navbar when user scroll down to show navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,32 @@
+"use client"
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import ThemeSwitch from './ThemeSwitcher';
 
 export default function Navbar() {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 250) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="navbar ">
+    <div className={`navbar z-50 ${isSticky ? 'fixed top-0 left-0 w-full  bg-secondary-200' : ''}`}>
       <div className="navbar-start flex justify-between w-full">
         <h1 className="font-bold text-2xl text-primary">Daily thoughts</h1>
-        <div className="dropdown dropdown-end lg:hidden ">
-          <div tabIndex={0} role="button" className="btn btn-ghost ">
+        <div className="dropdown dropdown-end lg:hidden">
+          <div tabIndex={0} role="button" className="btn btn-ghost">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5 rotate-180"
@@ -25,7 +44,7 @@ export default function Navbar() {
           </div>
           <ul
             tabIndex={0}
-            className="bg-primary text-primary-300 menu menu-sm dropdown-content dropdown-left rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            className={`bg-secondary-200  menu menu-sm dropdown-content dropdown-left rounded-box z-10 mt-3 w-52 p-2 shadow`}
           >
             <li>
               <Link href={'/'}>Home</Link>
@@ -43,7 +62,7 @@ export default function Navbar() {
         </div>
       </div>
       <div className="navbar-end hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+        <ul className="menu menu-horizontal p-0">
           <li>
             <Link href={'/'}>Home</Link>
           </li>
