748e2557e98bfae4517922186cc6d2a3a5be3c7f
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-01-02 19:22:39 +0330

    update(page): landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Advice from "@/components/Advice";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import ScrollIndicator from "@/components/ScrollIndicator"
 
 export default function Home() {
   return (
     <div id="root" className="h-full container m-auto flex flex-col text-primary-200">
       <Navbar />
+      <ScrollIndicator />
       <main className="h-full">
         <Hero />
         {Array.from({ length: 2 }, ((_, i) => <Advice key={i} />))}
