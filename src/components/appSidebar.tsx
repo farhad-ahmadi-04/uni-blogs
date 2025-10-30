8f6e7ad1c929a10aed4789049cfcbeb7cb9f9738
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-10-30 12:38:09 +0330

    feat: implement AppSidebar component with dynamic menu items

---
+++ src/components/appSidebar.tsx
@@ -0,0 +1,46 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import menuItems from "@/lib/menuItem";
+import Link from "next/link";
+
+function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <Link href={"/"} className="text-lg font-bold">
+          uni-Blogs
+        </Link>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {menuItems.map((item, index) => (
+                <SidebarMenuItem key={index}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.href}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
+
+export default AppSidebar;
