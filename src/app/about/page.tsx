fa88d9f3365dfb6d766c99f1eccdd51ad0338819
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-10-30 12:53:38 +0330

    feat: create About page with semantic structure and content

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/ui/container";
+
+function About() {
+    return ( <section>
+        <Container>
+            <h1 className="text-3xl font-bold">About Us</h1>
+            <p className="mt-4">Welcome to uni-blogs, a platform where you can find and share knowledge on various topics.</p>
+        </Container>
+    </section> );
+}
+
+export default About;
