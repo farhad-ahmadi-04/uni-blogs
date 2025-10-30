606772147b14dd49498407a7e748db313092ab38
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-10-30 12:38:01 +0330

    feat: add menuItems array for navigation structure

---
+++ src/lib/menuItem.ts
@@ -0,0 +1,26 @@
+import { BookOpenText, Contact, Home, Newspaper } from "lucide-react";
+
+// menu item
+const menuItems = [
+  {
+    title: "Home",
+    href: "/",
+    icon: Home,
+  },
+  {
+    title: "About",
+    href: "/about",
+    icon: BookOpenText ,
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+    icon: Newspaper 
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+    icon: Contact
+  },
+];
+export default menuItems;
