6622c36050a75e283f7f6e20123b7883247eee8f
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-10-30 12:53:43 +0330

    feat: add Blogs page with header and introductory content

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/ui/container";
+
+function Blogs() {
+    return ( <section>
+        <Container>
+            <h1 className="text-3xl font-bold">Blogs</h1>
+            <p className="mt-4">Explore our latest blogs and articles on various topics.</p>
+        </Container>
+    </section> );
+}
+
+export default Blogs;
