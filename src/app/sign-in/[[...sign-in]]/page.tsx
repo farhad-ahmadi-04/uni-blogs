f6ae084f682c475f42813382df20ae3d720947f0
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-11-12 14:55:44 +0330

    feat: enhance SignIn and SignUp pages with theme-aware appearance

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,19 @@
-import Container from "@/components/ui/container";
-import { SignIn } from "@clerk/nextjs";
+'use client'
 
-export default function Page() {
+import Container from "@/components/ui/container";
+import { useTheme } from "@/hooks/useContext";
+import { SignIn } from "@clerk/nextjs";
+import { dark, experimental__simple } from "@clerk/themes";
+
+export default function SignInPage() {
+const {isDark} = useTheme()
+
   return (
     <section>
       <Container className="flex justify-center items-center mt-10">
-        <SignIn />
+        <SignIn appearance={{
+                baseTheme: isDark ? dark : experimental__simple
+              }}/>
       </Container>
     </section>
   );
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,19 @@
-import Container from "@/components/ui/container";
-import { SignUp } from "@clerk/nextjs";
+'use client'
 
-export default function Page() {
+import Container from "@/components/ui/container";
+import { useTheme } from "@/hooks/useContext";
+import { SignUp } from "@clerk/nextjs";
+import { dark, experimental__simple } from "@clerk/themes";
+
+export default function SignUpPage() {
+const {isDark} = useTheme()
+
   return (
     <section>
-      <Container className="flex justify-center items-center mt-10">
-        <SignUp />
+      <Container className="flex justify-center items-center my-5">
+        <SignUp appearance={{
+                baseTheme: isDark ? dark : experimental__simple
+              }}/>
       </Container>
     </section>
   );
