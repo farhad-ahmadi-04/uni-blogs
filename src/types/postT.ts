e302a0258aa027c4a9c3d14c42e7b4ab6a9eca44
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-12-12 20:12:04 +0330

    feat: add post type

---
+++ src/types/postT.ts
@@ -0,0 +1,13 @@
+
+export interface postInterface {
+  _id: string;
+  userId: string;
+  content: string;
+  title: string;
+  image:string;
+    category: string,
+  slug: string,
+  createdAt: string,
+  updatedAt: string,
+  __v: number,
+};
