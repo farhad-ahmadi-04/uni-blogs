e4b7d52c415a0a9aebd106be36789051c54492dd
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-12-12 20:11:34 +0330

    refactor: update BlogCard component to accept post data as props and improve structure

--- src/components/blogCard.tsx
+++ src/components/blogCard.tsx
@@ -1,39 +1,34 @@
 import Link from "next/link";
 import { TypographyH3, TypographyP } from "./ui/typography";
 import { Image } from "@imagekit/next";
+import { postInterface } from "@/types/postT";
 
 /**
- * 
+ *
  * @returns create blog card component, that is also link to blog page
  */
-function BlogCard() {
+function BlogCard({ post }: { post: postInterface }) {
   return (
-    <Link href={"/"}>
-      <div className="border border-border">
-        <div className="flex items-center justify-center h-52 bg-blue-950">
-          <Image
-          src="/default-image.jpg"
+    <div className="border border-border">
+      <div className="flex items-center justify-center h-52 bg-blue-950">
+        <Image
+          src={post.image}
           width={300}
           height={200}
-          alt="picture of boy"
+          alt={post.title}
           className="w-full h-full bg-cover"
-          />
-        </div>
-        <div className="p-5 space-y-4">
-          <TypographyH3>
-            21 best free react components libraries to kickstart projects
-          </TypographyH3>
-          <TypographyP className="line-clamp-3">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Accusantium eveniet vitae omnis aliquam! Minima accusamus
-            consequuntur odit reiciendis vel nulla cum omnis necessitatibus,
-          </TypographyP>
-          <TypographyP className="first-letter:uppercase">
-            december 1, 2024
-          </TypographyP>
-        </div>
+        />
       </div>
-    </Link>
+      <div className="p-5 space-y-4">
+        <Link href={`/post/${post.slug}`}>
+          <TypographyH3>{post.title}</TypographyH3>
+        </Link>
+        <TypographyP className="line-clamp-3">{post.category}</TypographyP>
+        <TypographyP className="first-letter:uppercase">
+          {new Date(post.createdAt).toLocaleDateString()}
+        </TypographyP>
+      </div>
+    </div>
   );
 }
 
