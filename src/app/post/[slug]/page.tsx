9c6d9758e5aedb7f38eae98349bc361e3320a83b
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-12-12 18:23:00 +0330

    feat: create PostPage component for displaying individual blog posts

---
+++ src/app/post/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/ui/container";
+
+function PostPage() {
+  return (
+    <section>
+      <Container>
+        <h1>Post page</h1>
+      </Container>
+    </section>
+  );
+}
+
+export default PostPage;
