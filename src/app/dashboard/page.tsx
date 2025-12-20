27fd75010ba7ed4660056aac52b82d63944e27a2
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-12-20 23:31:37 +0330

    feat: add logic to get url for get render from component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,33 @@
-import Container from "@/components/ui/container";
+"use client";
 
-function Dashboard() {
-    return ( <section>
-      <Container>Dashboard page</Container>
-    </section> );
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import DashProfile from "@/components/dashProfile";
+import DashPosts from "@/components/dashPosts";
+import DashUsers from "@/components/dashUsers";
+import DashboardComp from "@/components/dashboardComp";
+
+export default function Dashboard() {
+  const searchParams = useSearchParams();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(searchParams);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [searchParams]);
+
+  return (
+    <div className="min-h-screen w-full my-5">
+      {/* profile... */}
+      {tab === "profile" && <DashProfile />}
+
+      {tab === "posts" && <DashPosts />}
+
+      {tab === "users" && <DashUsers />}
+      {tab === "dash" && <DashboardComp />}
+    </div>
+  );
 }
-
-export default Dashboard;
