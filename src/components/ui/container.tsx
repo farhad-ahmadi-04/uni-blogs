5910d64d82cfa4c4b8673c6a20cb38f41a02f468
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-12-20 23:39:27 +0330

    refactor: remove container and handle width by max width

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -19,7 +19,7 @@ import { cn } from "@/lib/utils";
  */
 function Container({ children, className }: { children: React.ReactNode, className?: string }) {
     return (
-        <div className={cn("w-full container xl:max-w-7xl mx-auto px-4", className)}>
+        <div className={cn("w-full  xl:max-w-7xl mx-auto px-4", className)}>
             {children}
         </div>
     );
