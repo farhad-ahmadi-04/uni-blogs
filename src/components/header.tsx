7bec2f1562da039366e3b7091eae46166a9fe3ee
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-12-20 23:40:55 +0330

    refactor: add menu of dashboard and handle loading and signed in of user and add active class to current item base on current url

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,10 +7,17 @@ import menuItems from "@/lib/menuItem";
 import { useCallback, useEffect, useState } from "react";
 import { Input } from "./ui/input";
 import { AnimatedThemeToggler } from "./ui/animated-theme-toggler";
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  SignedIn,
+  SignedOut,
+  SignOutButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
 import { useTheme } from "@/hooks/useContext";
 import { dark, experimental__simple } from "@clerk/themes";
-import { useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { TypographyP } from "./ui/typography";
 
 function Header() {
   const { isDark } = useTheme();
@@ -18,6 +25,21 @@ function Header() {
   const [searchItem, setSearchItem] = useState("");
   const searchParams = useSearchParams();
   const router = useRouter();
+  const { isSignedIn, user } = useUser();
+  const [tab, setTab] = useState("");
+  const url = usePathname()
+  
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(searchParams);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }else {
+      setTab("");
+    }
+  }, [searchParams]);
+
 
   const handleSearch = useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
@@ -60,7 +82,7 @@ function Header() {
         </div>
 
         <nav
-          className={`block md:hidden absolute z-10 left-0 right-0 top-[72px] transition-all ease-in-out duration-300 transform ${
+          className={`block md:hidden absolute z-10 left-0 right-0 top-[68px] transition-all ease-in-out duration-300 transform ${
             isOpen ? "bottom-0" : "bottom-full"
           } bg-background`}
         >
@@ -69,11 +91,59 @@ function Header() {
               isOpen ? "opacity-100" : "opacity-0"
             } flex h-full flex-col justify-center items-center gap-4 text-2xl font-semibold`}
           >
+             {/* menu of header */}
             {menuItems.map((item) => (
               <li key={item.title} onClick={() => setIsOpen(!isOpen)}>
-                <Link href={item.href}>{item.title}</Link>
+                <Link href={item.href} className={`${url === item.href && "text-ring"}`}>{item.title}</Link>
               </li>
             ))}
+             <TypographyP className="text-muted-foreground font-medium text-xl">--Dashboard menu--</TypographyP>
+             {/* add dashboard menu when user is signed in and open menu */}
+            {isSignedIn && (user.publicMetadata.isAdmin as boolean) && (
+              <li onClick={() => setIsOpen(!isOpen)}>
+                  <Link
+                    href="/dashboard?tab=dash"
+                    className={`${tab === "dash" && url === "/dashboard" && "text-ring"} w-full flex items-center gap-2`}
+                  >
+                    Dashboard
+                  </Link>
+              </li>
+            )}
+            <li onClick={() => setIsOpen(!isOpen)}>
+                <Link
+                  href="/dashboard?tab=profile"
+                  className={`${tab === "profile" && url === "/dashboard" && "text-ring" } w-full flex items-center justify-between gap-2`}
+                >
+                  <div className="flex items-center gap-2">Profile</div>
+                  <div className="text-sm font-medium text-muted-foreground">
+                    {user?.publicMetadata?.isAdmin ? "Admin" : "User"}
+                  </div>
+                </Link>
+            </li>
+            {(user?.publicMetadata?.isAdmin as boolean) && (
+              <li onClick={() => setIsOpen(!isOpen)}>
+                  <Link
+                    href="/dashboard?tab=posts"
+                    className={`${tab === "posts" && url === "/dashboard" && "text-ring"} w-full flex items-center gap-2`}
+  
+                  >
+                    Posts
+                  </Link>
+              </li>
+            )}
+            {(user?.publicMetadata?.isAdmin as boolean) && (
+              <li onClick={() => setIsOpen(!isOpen)}>
+                  <Link
+                    href="/dashboard?tab=users"
+                    className={`${tab === "users" && url === "/dashboard" && "text-ring"} w-full flex items-center gap-2`}
+                  >
+                    Users
+                  </Link>
+              </li>
+            )}
+            <li  onClick={() => setIsOpen(!isOpen)}>
+                <SignOutButton />
+            </li>
           </ul>
         </nav>
         <Link
@@ -85,11 +155,18 @@ function Header() {
         {/* desktop menu */}
         <nav className="hidden md:block">
           <ul className="flex gap-8 lg:gap-10">
+            {/* menu of header */}
             {menuItems.map((item) => (
               <li key={item.title} className="lg:text-lg">
                 <Link href={item.href}>{item.title}</Link>
               </li>
             ))}
+            {/* add dashboard link when user is signed in */}
+            {isSignedIn && (
+              <li className="lg:text-lg">
+                <Link href={"/dashboard"}>Dashboard</Link>
+              </li>
+            )}
           </ul>
         </nav>
         <div>
