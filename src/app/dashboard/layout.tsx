dfa033b0258a96175ec4d7f8ec766b6546d661f5
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-12-21 00:09:33 +0330

    refactor: wrap children to suspense

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,14 +3,15 @@
 import DashboardSidebar from "@/components/dashSidebar";
 import Container from "@/components/ui/container";
 import { TypographyH1 } from "@/components/ui/typography";
-import {  useUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
+import { Suspense } from "react";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { user, isSignedIn, isLoaded } = useUser();
+  const { isSignedIn, isLoaded } = useUser();
 
   if (!isLoaded) return null;
   if (!isSignedIn) {
@@ -22,9 +23,9 @@ export default function DashboardLayout({
       <section className="mb-5">
         <Container className="flex md:flex-col lg:flex-row px-0  border-t-2 border-t-border">
           <div className="py-5 px-1 hidden md:flex md:w-full md:flex-row lg:flex-col lg:w-52 lg:border-r-2 lg:border-r-border bg-background">
-           <DashboardSidebar/>
+            <DashboardSidebar />
           </div>
-          {children}
+          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
         </Container>
       </section>
     );
