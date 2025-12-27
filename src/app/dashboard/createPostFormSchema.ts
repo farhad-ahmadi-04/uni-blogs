c351856e3eac552313bf1e8ae7fe9459314a39b0
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-12-27 17:03:05 +0330

    feat: add form schema for post creation with validation rules

---
+++ src/app/dashboard/createPostFormSchema.ts
@@ -0,0 +1,31 @@
+import z from "zod";
+
+export const category = [
+  "ui",
+  "ux",
+  "ai",
+  "develop",
+  "design",
+  "marketing",
+] as const;
+
+export const formSchema = z.object({
+  title: z.string().min(3, {
+    error: "Too small: expected string to have more than 3 characters",
+  }),
+  category: z.string()
+  .refine((val) => val !== "category", {
+    message: "Please select a category",
+  }),
+  image: z
+    .any()
+    .refine((files) => files?.length === 1, "Please upload an image")
+    .refine(
+      (files) =>
+        ["image/png", "image/jpeg", "image/jpg"].includes(files?.[0]?.type),
+      "Only PNG / JPG allowed"
+    )
+    .refine((files) => files?.[0]?.size <= 5 * 1024 * 1024, "Max size is 5MB"),
+  content: z.string(),
+});
+export type formSchemaType = z.infer<typeof formSchema>;
