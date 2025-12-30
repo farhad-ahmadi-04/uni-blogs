2feb0774b121f2f1a3fa99cc4d98f23c38026908
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-12-30 23:01:18 +0330

    feat: update postInterface formatting and add userInterface definition

--- src/types/postT.ts
+++ src/types/postT.ts
@@ -1,13 +1,12 @@
-
 export interface postInterface {
   _id: string;
   userId: string;
   content: string;
   title: string;
-  image:string;
-    category: string,
-  slug: string,
-  createdAt: string,
-  updatedAt: string,
-  __v: number,
-};
+  image: string;
+  category: string;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
---
+++ src/types/userT.ts
@@ -0,0 +1,13 @@
+export interface userInterface {
+  clerkId: string;
+  createdAt: string;
+  email: string;
+  firstName: string;
+  isAdmin: boolean;
+  lastName: string;
+  profilePicture: string;
+  updatedAt: string;
+  username: string;
+  __v: number;
+  _id: string;
+}
