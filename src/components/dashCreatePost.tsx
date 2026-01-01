f73502219544e9aeea067b121100bfa8aabf8fe3
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2026-01-01 20:42:18 +0330

    feat: add DashCreatePost component for admin post creation and authorization handling

---
+++ src/components/dashCreatePost.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Container from "@/components/ui/container";
+import { useUser } from "@clerk/nextjs";
+import CreatePostForm from "./createPostForm";
+
+function DashCreatePost() {
+  const { isSignedIn, user, isLoaded } = useUser();
+
+  if (!isLoaded) return null;
+
+  if (isSignedIn && user.publicMetadata.isAdmin) {
+    return (
+      <section className="w-full">
+        <Container className="flex justify-center mt-3">
+          <CreatePostForm />
+        </Container>
+      </section>
+    );
+  } else {
+    return (
+      <section>
+        <Container >
+          <h1 className="text-center text-4xl font-semibold">
+            You are not authorized to view this page
+          </h1>
+        </Container>
+      </section>
+    );
+  }
+}
+
+export default DashCreatePost;
