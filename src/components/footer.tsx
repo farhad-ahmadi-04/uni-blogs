62cb0dae929ba3f84f749ebd5ed4f707ba6ef4bc
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2026-01-01 20:41:17 +0330

    refactor: replace div with Container component in Footer for improved structure

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,15 @@
+import Container from "./ui/container";
+
 function Footer() {
-    return ( <footer>
-        <div className="w-full h-20 flex items-center justify-center border-t mt-10">
-            <p className="text-sm text-gray-500">
-                &copy; {new Date().getFullYear()} Uni-Blogs. All rights reserved.
-            </p>
-        </div>
-    </footer> );
+  return (
+    <footer>
+      <Container className="h-20 flex items-center justify-center border-t mt-10">
+        <p className="text-sm text-gray-500">
+          &copy; {new Date().getFullYear()} Uni-Blogs. All rights reserved.
+        </p>
+      </Container>
+    </footer>
+  );
 }
 
 export default Footer;
